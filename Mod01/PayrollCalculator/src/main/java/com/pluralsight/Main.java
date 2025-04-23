78a1fb9dd5f06c157eccdc171f3e5fc8e7c70d7f
Maya Thompson <mayaandromedathompson@gmail.com>
2025-04-23 12:00:41 -0500

    Workbook 3-2-1 added FileWriter to create a calculated payroll document

--- Mod01/PayrollCalculator/src/main/java/com/pluralsight/Main.java
+++ Mod01/PayrollCalculator/src/main/java/com/pluralsight/Main.java
@@ -1,36 +1,45 @@
 package com.pluralsight;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.FileReader;
+import java.io.FileWriter;
 
 public class Main {
 
     public static void main(String[] args) {
 
+        String fromFile = Utils.messageAndResponse("What file would you like to process? ");
+        String toFile = Utils.messageAndResponse("What would you like to call the output file? ");
+
         FileReader reader;
+        FileWriter writer;
         try {
-            reader = new FileReader("/c:/Users/ru551/pluralsight/workbook-3/Mod01/PayrollCalculator/DataFiles/employees.csv");
             String input;
+
+            reader = new FileReader("/c:/Users/ru551/pluralsight/workbook-3/Mod01/PayrollCalculator/DataFiles/" + fromFile);
+            writer = new FileWriter("/c:/Users/ru551/pluralsight/workbook-3/Mod01/PayrollCalculator/DataFiles/" + toFile);
+
             BufferedReader bufReader = new BufferedReader(reader);
+            BufferedWriter bufWriter = new BufferedWriter(writer);
 
             Employee[] employees = new Employee[8];
             int lineIndex = 0;
             while ((input = bufReader.readLine()) != null) {
                 String[] lineData = input.split("\\|");
                 if (lineData[0].equals("id")) {
+                    bufWriter.write("id|name|gross pay");
                     continue;
                 }
 
-                int id= Integer.parseInt(lineData[0]);
-                String name = lineData[1];
-                double hours = Double.parseDouble(lineData[2]);
-                double rate = Double.parseDouble(lineData[3]);
-                employees[lineIndex] = new Employee(id, name, hours, rate);
-                System.out.printf("ID: %d \nName: %s \nGross Pay: $%.2f\n\n", employees[lineIndex].getEmployeeId(), employees[lineIndex].getName(), employees[lineIndex].calculateGrossPay());
+                employees[lineIndex] = new Employee(Integer.parseInt(lineData[0]), lineData[1], Double.parseDouble(lineData[2]), Double.parseDouble(lineData[3]));
+                bufWriter.write("\n" + employees[lineIndex].getEmployeeId() + "|" + employees[lineIndex].getName() + "|" + employees[lineIndex].calculateGrossPay());
                 lineIndex++;
             }
             bufReader.close();
-        } catch (Exception e) {
+            bufWriter.close();
+        }
+        catch (Exception e) {
             System.err.println("There was a problem: " + e.getMessage());
         }
 
